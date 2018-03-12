6792fa0444dbc16c31ab9b35c844b403ebcb0a31
wjdtjq0630 <wjdtjq0630@gmail.com>
2018-03-12 21:16:04 +0900

    php 인코딩 설정

--- config.php
+++ config.php
@@ -9,6 +9,7 @@
   mysqli_query($conn, "set session character_set_connection=utf8;");
   mysqli_query($conn, "set session character_set_results=utf8;");
   mysqli_query($conn, "set session character_set_client=utf8;");
+  header("Content-Type: text/html; charset=UTF-8");
 
   if(mysqli_connect_errno($conn)){
     echo "데이터베이스 연결 실패:".mysqli_connect_error();
